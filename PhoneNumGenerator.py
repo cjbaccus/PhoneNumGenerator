b04a6298ddc01640e3a111711e72009d02b8ff15
cjbaccus <cjbaccus@gmail.com>
2017-08-22 06:09:37 -0700

    added command line raw_input for state and city

--- PhoneNumGenerator.py
+++ PhoneNumGenerator.py
@@ -7,20 +7,12 @@ import pandas as pd
 import requests
 import re
 from bs4 import BeautifulSoup
-from tabulate import tabulate
+      
+state = raw_input("What state?")
+City = raw_input("What City?")
 
-#Main function
-#areaCodes = ["669205", "669327", "408413", "408422", "408427", "408430", "408442", "669500", "408632", "408665", "408713", "408767", "408840", "408842", "408843", "408846", "408847", "408848", "408852"]
-
-# for ac in areaCodes:
-#     for num in range(1, 9999):
-#         lz = format(num, '04d')
-#         print(ac + lz)
-#         num = num + 1
-#         
-
-state = "CA"
-City = "Gilroy"
+# state = "CA"
+# City = "Gilroy"
 lookup = "https://www.area-codes.com/city/city.asp?state="+ state + "&city=" + City
 print(lookup)
 
